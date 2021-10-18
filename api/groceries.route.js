e3d1858a80fe6a5621f1189be06e3372e068bb23
mAtencio27 <mAtencio27@hotmail.com>
2021-10-18 22:04:44 +0900

    first commit

--- api/groceries.route.js
+++ api/groceries.route.js
@@ -2,6 +2,6 @@ import express from "express"
 
 const router = express.Router()
 
-router.route("/").get((req,res) => res.send("ughhhhh"))
+router.route("/").get((req,res) => res.send("ughhhhh!"))
 
 export default router
